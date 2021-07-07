2f28cd7519030c66310cf295d016ee8c7d66b78d
ZsoltD <zsolt.deak@betssongroup.com>
2021-07-07 09:52:21 +0200

    new approve button added

--- userscript.js
+++ userscript.js
@@ -5,12 +5,32 @@
 // @description  Adds an approve button
 // @author       ZsoltD
 // @match        https://github.com/*/pull/*
-// @icon         https://www.google.com/s2/favicons?domain=github.com 
+// @icon         https://www.google.com/s2/favicons?domain=github.com
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    // Your code here...
+    function approve() {
+        const a = $("a[href$='pull/2/files']");
+        $("a[href$='pull/2/files']")[0].click();
+        var checkExist = setInterval(function () {
+            const approveBtn = $("button[value='approve']");
+            if (approveBtn.length) {
+                approveBtn[0].click();
+                clearInterval(checkExist);
+            }
+        }, 100);
+    }
+
+    $(() => {
+        var checkExist = setInterval(function () {
+            if (!$("#zsolt-approve-button").length) {
+                $(".gh-header-actions")
+                    .prepend(`<button id="zsolt-approve-button" class="btn btn-sm btn-primary">Approve</button>`)
+                    .click(() => approve());
+            }
+        }, 1000);
+    });
 })();
