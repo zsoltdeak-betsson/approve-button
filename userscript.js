4f5b4719bb7861e0b610f3874e6210e9540043a7
ZsoltD <zsolt.deak@betssongroup.com>
2021-07-07 09:11:33 +0200

    first commit

---
+++ userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Approve Button
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds an approve button
+// @author       ZsoltD
+// @match        https://github.com/*/pull/*
+// @icon         https://www.google.com/s2/favicons?domain=github.com 
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+})();
